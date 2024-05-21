98d64b7dcf38f106134087f0e9932499fbf3e142
Kuroyukimaru <oranggabut712@gmail.com>
2024-05-21 11:21:25 +0700

    menambahkan fungsi main yang membuat objek mahasiswa

--- ParadigmaOOP1_0151/MemberInitList.cpp
+++ ParadigmaOOP1_0151/MemberInitList.cpp
@@ -19,7 +19,9 @@ public:
         cout << "Nilai =" << nilai << endl;
     }
 };
-int main()
-{
-    std::cout << "Hello World!\n";
+
+int main(){
+    mahasiswa mhs(12, "Asroni", 90.5);
+
+    return 0;
 }
